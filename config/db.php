209dfcd6e0746c314cdced2f1de1f2526d68561b
NguyenVuong-hub <vle00882@gmail.com>
2026-05-01 22:17:43 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,24 +5,10 @@ $pass     = getenv('DB_PASS');
 $dbname   = getenv('DB_NAME');
 $port     = (int) getenv('DB_PORT');
 
-$conn = mysqli_init();
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
-// Bỏ qua xác thực certificate
-mysqli_ssl_set($conn, null, null, null, null, null);
-
-mysqli_real_connect(
-    $conn,
-    $host,
-    $user,
-    $pass,
-    $dbname,
-    $port,
-    null,
-    MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT
-);
-
-if (mysqli_connect_errno()) {
-    die("Kết nối thất bại: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
 }
 
 mysqli_set_charset($conn, "utf8mb4");
